9773b071e1a0da4a2e222775d8721c44a15ce402
Ewald484 <ewaldsnyman1@gmail.com>
2025-12-10 15:18:54 +0200

    SignUp and login page components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignUpPage } from './sign-up-page/sign-up-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'SignUpPage', pathMatch: 'full' },
+    { path: 'SignUp', component: SignUpPage }
+];
